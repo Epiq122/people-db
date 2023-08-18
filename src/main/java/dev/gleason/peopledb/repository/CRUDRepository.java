1e1456d3ebade3b5c67828a9fcf67aaaf93d32c2
Robert Gleason <l2ob122@gmail.com>
2023-08-17 21:43:58 -0700

    moved all the other code to the master class

--- src/main/java/dev/gleason/peopledb/repository/CRUDRepository.java
+++ src/main/java/dev/gleason/peopledb/repository/CRUDRepository.java
@@ -3,7 +3,11 @@ package dev.gleason.peopledb.repository;
 import dev.gleason.peopledb.model.Entity;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 abstract class CRUDRepository<T extends Entity> {
     protected Connection connection; // can be seen by subclass like PeopleRepository
@@ -51,6 +55,93 @@ abstract class CRUDRepository<T extends Entity> {
         return Optional.ofNullable(entity);
     }
 
+
+    public List<T> findAll() {
+        List<T> entites = new ArrayList<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(getFindAllSql());
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                entites.add(extractEntityFromResultSet(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return entites;
+    }
+
+    public long count() {
+        long count = 0;
+        try {
+            PreparedStatement ps = connection.prepareStatement(getCountSql());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                count = rs.getLong(1);
+                System.out.printf("Total Count: %s%n", count);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    public void delete(T entity) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(getDeleteSql());
+            ps.setLong(1, entity.getId());
+            int deletedRecordCount = ps.executeUpdate();
+            System.out.printf("Deleted Records: %s%n", deletedRecordCount);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void delete(T... entites) {
+        try {
+            Statement stmt = connection.createStatement();
+            String ids = Arrays.
+                    stream(entites)
+                    .map(T::getId)
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(","));
+            int deletedRecordsCount = stmt.executeUpdate(getDeleteInSql().replace(":ids", ids));
+            System.out.println("Deleted Records: " + deletedRecordsCount);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void update(T entity) {
+        try {
+            PreparedStatement ps = connection.prepareStatement(getUpdateSql());
+            mapForUpdate(entity, ps);
+            ps.setLong(5, entity.getId());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    protected abstract String getUpdateSql();
+
+    /**
+     * @return Should return a SQL string like: DELETE "FROM PEOPLE WHERE ID IN (:ids)"
+     * Be sure to include the '(:ids)' named parameter  and call it ids;
+     */
+
+    protected abstract String getDeleteInSql();
+
+    protected abstract String getDeleteSql();
+
+    protected abstract String getCountSql();
+
+    protected abstract String getFindAllSql();
+
     abstract T extractEntityFromResultSet(ResultSet rs) throws SQLException;
 
     /**
@@ -62,6 +153,7 @@ abstract class CRUDRepository<T extends Entity> {
 
     abstract void mapForSave(T entity, PreparedStatement ps) throws SQLException;
 
+    abstract void mapForUpdate(T entity, PreparedStatement ps) throws SQLException;
 
     abstract String getSaveSql();
 
--- src/main/java/dev/gleason/peopledb/repository/PeopleRepository.java
+++ src/main/java/dev/gleason/peopledb/repository/PeopleRepository.java
@@ -6,17 +6,15 @@ import java.math.BigDecimal;
 import java.sql.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 
 public class PeopleRepository extends CRUDRepository<Person> {
     private static final String INSERT_PERSON_SQL = "INSERT INTO PEOPLE (FIRST_NAME, LAST_NAME, DOB) VALUES (?, ?, ?)";
     public static final String FIND_BY_ID_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE WHERE ID = ?";
     public static final String SELECT_COUNT_SQL = "SELECT COUNT(*) FROM PEOPLE";
     public static final String FIND_ALL_SQL = "SELECT ID, FIRST_NAME, LAST_NAME, DOB, SALARY FROM PEOPLE";
+    public static final String DELETE_SQL = "DELETE FROM PEOPLE WHERE ID = ?";
+    public static final String UPDATE_SQL = "UPDATE PEOPLE SET FIRST_NAME = ?, LAST_NAME = ?, DOB = ?, SALARY = ? WHERE ID = ?";
+    public static final String DELETE_IN_SQL = "DELETE FROM PEOPLE WHERE ID IN (:ids)";
     private PreparedStatement ps;
 
 
@@ -48,83 +46,45 @@ public class PeopleRepository extends CRUDRepository<Person> {
     }
 
     @Override
+    void mapForUpdate(Person entity, PreparedStatement ps) throws SQLException {
+        ps.setString(1, entity.getFirstName());
+        ps.setString(2, entity.getLastName());
+        ps.setTimestamp(3, convertDobToTimestamp(entity.getDateOfBirth()));
+        ps.setBigDecimal(4, entity.getSalary());
+        ps.setLong(5, entity.getId());
+    }
+
+
+    @Override
     String getFindBySql() {
         return FIND_BY_ID_SQL;
     }
 
-
-    public List<Person> findAll() {
-        List<Person> people = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(FIND_ALL_SQL);
-            ResultSet rs = ps.executeQuery();
-            while (rs.next()) {
-                people.add(extractEntityFromResultSet(rs));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return people;
+    @Override
+    protected String getFindAllSql() {
+        return FIND_ALL_SQL;
     }
 
-
-    public long count() {
-        long count = 0;
-        try {
-            PreparedStatement ps = connection.prepareStatement(SELECT_COUNT_SQL);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                count = rs.getLong(1);
-                System.out.printf("Total Count: %s%n", count);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return count;
+    @Override
+    protected String getCountSql() {
+        return SELECT_COUNT_SQL;
     }
 
-    public void delete(Person person) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("DELETE FROM PEOPLE WHERE ID = ?");
-            ps.setLong(1, person.getId());
-            int deletedRecordCount = ps.executeUpdate();
-            System.out.printf("Deleted Records: %s%n", deletedRecordCount);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected String getDeleteSql() {
+        return DELETE_SQL;
     }
 
-
-    public void delete(Person... people) {
-        try {
-            Statement stmt = connection.createStatement();
-            String ids = Arrays.
-                    stream(people)
-                    .map(Person::getId)
-                    .map(String::valueOf)
-                    .collect(Collectors.joining(","));
-            int deletedRecordsCount = stmt.executeUpdate("DELETE FROM PEOPLE WHERE ID IN (:ids)".replace(":ids", ids));
-            System.out.println("Deleted Records: " + deletedRecordsCount);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected String getDeleteInSql() {
+        return DELETE_IN_SQL;
     }
 
-    public void update(Person person) {
-        try {
-            PreparedStatement ps = connection.prepareStatement("UPDATE PEOPLE SET FIRST_NAME = ?, LAST_NAME = ?, DOB = ?, SALARY = ? WHERE ID = ?");
-            ps.setString(1, person.getFirstName());
-            ps.setString(2, person.getLastName());
-            ps.setTimestamp(3, convertDobToTimestamp(person.getDateOfBirth()));
-            ps.setBigDecimal(4, person.getSalary());
-            ps.setLong(5, person.getId());
-            ps.executeUpdate();
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @Override
+    protected String getUpdateSql() {
+        return UPDATE_SQL;
     }
+
 
     private static Timestamp convertDobToTimestamp(ZonedDateTime dob) {
         return Timestamp.valueOf(dob.withZoneSameInstant(ZoneId.of("+0")).toLocalDateTime());
